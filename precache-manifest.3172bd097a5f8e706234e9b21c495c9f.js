5c5fcd8825e3795feb8842d7942765771fc5b68c
sidx8 <siddharth0sinha@gmail.com>
2020-09-06 21:36:30 +0530

    Updates

---
+++ precache-manifest.3172bd097a5f8e706234e9b21c495c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c30fb8e522bd2b7076005ad5274630b",
+    "url": "/React-Todo-app/index.html"
+  },
+  {
+    "revision": "ad7bed24f2b1ff17f3d9",
+    "url": "/React-Todo-app/static/css/main.4bbcd286.chunk.css"
+  },
+  {
+    "revision": "59fc3a8c5f81cba7ba67",
+    "url": "/React-Todo-app/static/js/2.e5e056a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Todo-app/static/js/2.e5e056a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7bed24f2b1ff17f3d9",
+    "url": "/React-Todo-app/static/js/main.25bf00be.chunk.js"
+  },
+  {
+    "revision": "d1ff515975ca47c14ce7",
+    "url": "/React-Todo-app/static/js/runtime-main.c27f6023.js"
+  }
+]);
